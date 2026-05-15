507932e2043a222d2b761746bec64869ee7b2423
Monica Davila <monica.davila510@gmail.com>
2026-05-14 21:29:19 -0500

    Add middleware and movie routes

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 // dependencies
 require("dotenv").config();
 const express = require("express");
+const movieRoutes = require("./routes/movieRoutes");
 
 // app configuration
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// middleware
+app.use(express.json());
+
+// routes
+app.use("/api", movieRoutes);
+
 // server listener
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Movie Finder API is running on http://localhost:${PORT}`);
 });
